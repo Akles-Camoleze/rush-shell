84f2068b921033efd86d20d4fac44b4ac218898f
Joao-Lamounier <jjoaolamounier@gmail.com>
2023-05-07 14:33:51 -0300

    Implementada a função para exibição de mensagens

--- views/messages/messages.view.h
+++ views/messages/messages.view.h
@@ -1,4 +1,9 @@
 #ifndef RUSH_SHELL_MESSAGES_VIEW_H
 #define RUSH_SHELL_MESSAGES_VIEW_H
 
+#include <stdbool.h>
+#include <stdio.h>
+
+void get_message(char *, bool);
+
 #endif //RUSH_SHELL_MESSAGES_VIEW_H
