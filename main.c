82fdb8e19e6ab178313a67da0b375881af755667
Akles-Camoleze <aklespires@gmail.com>
2023-05-08 00:34:06 -0300

    Implementada função para split de string e checagem para saída do programa

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
+#include "services/commandline/commandline.service.h"
+#include "views/messages/messages.view.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char *command = NULL;
+    while (1) {
+        free(command);
+        command = NULL;
+        get_message("\033[35;1mrush@shell\033[37;1m:\033[32m~\033[37m$ ", false);
+        get_command_line(&command);
+        bool checked = check_command(command);
+        if (checked) {
+            get_message(strcat(command, "\n"), false);
+        }
+    }
 
     return 0;
 }
--- views/messages/messages.view.c
+++ views/messages/messages.view.c
@@ -1,5 +1,5 @@
 #include "messages.view.h"
 
 void get_message(char *_msg, bool _error) {
-    _error ? perror(_msg) : printf("\n%s", _msg);
+    _error ? perror(_msg) : printf("%s", _msg);
 }
