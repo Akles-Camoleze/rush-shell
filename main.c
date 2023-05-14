7fb26f6e7945d2a75d32afc0a0410de192b6a2c2
Akles-Camoleze <aklespires@gmail.com>
2023-05-14 15:11:27 -0300

    Iniciando pipes com reaproveitamento

--- main.c
+++ main.c
@@ -12,63 +12,62 @@
 
 int main() {
     char *command = NULL;
-    int pipes[2][2];
-    /*
-     * n % 2 == 0: PipeA
-     * n % 2 == 1: PipeB
-     * [
-     *      0: PipeA = []
-     *      1: PipeB = []
-     * ]
-     */
-    pid_t p_ids[1000];
-    char *command_path;
-    pipe(pipes[A]);
-    pipe(pipes[B]);
-    char *test1[] = {"/usr/bin/ls", NULL};
-    char *test2[] = {"/usr/bin/wc", NULL};
 
     while (1) {
-        free(command);
-        command = NULL;
-        get_message("\033[35;1mrush@shell\033[37;1m:\033[32m~\033[37m$ ", false);
+        clean((void **) &command);
+        get_message("$ ", false);
         get_command_line(&command);
         bool checked = check_command(command);
-
         if (checked) {
-            // explode em pipe
             int n_args;
             char **command_split = split_command(command, "|", &n_args); // matriz do split em pipe
             check_exit(*command_split);
+            int pipes[2][2];
+            pipe(pipes[A]);
+            pipe(pipes[B]);
+            pid_t p_ids[n_args];
 
-            // recebe caminho do programa
-            command_path = get_command_path(*command_split);
-            if (check_command(command_path)) {
-                *command_split = realloc_command_line(*command_split, strlen(command_path));
-                strcpy(*command_split, command_path);
-                clean((void **) &command_path);
-            }
-
-
-            // inicia split em dup
-            char **command_dup;
-            for (int i = 0; i < n_args; ++i) {
-                int n_dup_args;
-                command_dup = split_command(command_split[i], ">", &n_dup_args);
-
-                // inicia split em space
-                char **command_space;
-                for (int j = 0; j < n_dup_args; ++j) {
-                    int n_space_args;
-                    command_space = split_command(command_dup[i], " ", &n_space_args);
-                    p_ids[0] = fork();
-                    if (p_ids[0] == 0) {
-                        execvp(*command_space, command_space);
-                    }
+            for (int i = 0; i < n_args; i++) {
+                char *command_path = get_command_path(command_split[i]);
+                if (check_command(command_path)) {
+                    command_split[i] = realloc_command_line(command_split[i], strlen(command_path));
+                    strcpy(command_split[i], command_path);
+                    clean((void **) &command_path);
                 }
+                printf("\n%s", command_split[i]);
+                if (i >= 3) {
+                    waitpid(p_ids[i - 3], NULL, 0);
+                }
+                p_ids[i] = fork();
+                if (p_ids[i] == 0) {
+                    if (i == 0) {
+                        dup2(pipes[A][W], STDOUT_FILENO);
+                    } else {
+                        dup2(pipes[(i - 1) % 2][R], STDIN_FILENO);
+                        if (command_split[i + 1] != NULL) {
+                            dup2(pipes[i % 2][W], STDOUT_FILENO);
+                        }
+                    }
+                    char *teste[] = {command_split[i], NULL};
+                    close(pipes[A][R]);
+                    close(pipes[A][W]);
+                    close(pipes[B][R]);
+                    close(pipes[B][W]);
+                    execvp(command_split[i], teste);
+                    get_message(command_split[i], true);
+                    exit(EXIT_FAILURE);
+                }
+                waitpid(p_ids[(i + 1) % 2], NULL, 0);
+            }
+            close(pipes[B][W]);
+            close(pipes[B][R]);
+            close(pipes[A][R]);
+            close(pipes[A][W]);
+            waitpid(p_ids[n_args - 1], NULL, 0);
+            for (int i = 0; i < n_args; ++i) {
+                clean((void**) &command_split[i]);
             }
         }
-        wait(NULL);
     }
     return 0;
 }
